82d18e13c3cc6a73a6989c866646baad2419164e
akavity <basilakavity@gmail.com>
2023-02-24 00:09:46 +0300

    add Consumer

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/jobs/Consumer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/jobs/Consumer.java
@@ -1,4 +1,14 @@
 package com.rakovets.course.java.core.practice.concurrency.jobs;
 
-public class Consumer {
+import com.rakovets.course.java.core.practice.concurrency.dto.Queue;
+import com.rakovets.course.java.core.practice.concurrency.supplier.Producer;
+
+public class Consumer implements Runnable {
+    @Override
+    public void run() {
+        Queue queue = new Producer().getQueue();
+
+       // if (!)
+
+    }
 }
