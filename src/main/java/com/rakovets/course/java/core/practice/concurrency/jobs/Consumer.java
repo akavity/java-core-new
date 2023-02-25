5be2b8b51c305d743a42f038ab6496069be33cde
akavity <basilakavity@gmail.com>
2023-02-25 23:54:16 +0300

    fix: change constructor

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/jobs/Consumer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/jobs/Consumer.java
@@ -1,14 +1,36 @@
 package com.rakovets.course.java.core.practice.concurrency.jobs;
 
-import com.rakovets.course.java.core.practice.concurrency.dto.Queue;
-import com.rakovets.course.java.core.practice.concurrency.supplier.Producer;
+import com.rakovets.course.java.core.practice.concurrency.utils.FileUtil;
+
+import java.util.Queue;
+import java.util.concurrent.TimeUnit;
 
 public class Consumer implements Runnable {
+    private final Queue<Integer> queue;
+
+    public Consumer(Queue<Integer> queue) {
+        this.queue = queue;
+    }
+
     @Override
     public void run() {
-        Queue queue = new Producer().getQueue();
+        FileUtil file = new FileUtil("test.txt");
+        while (queue.peek() != null) {
+            int sleepTime = queue.poll();
+            sleep(sleepTime);
+            System.out.println(sleepTime + "slept");
+            file.write(" - I slept " + sleepTime + " second(s)");
+        }
+        System.out.println("1 second slept");
+        file.write(" ...");
+        sleep(1);
+    }
 
-       // if (!)
-
+    private void sleep(int time) {
+        try {
+            TimeUnit.SECONDS.sleep(time);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
